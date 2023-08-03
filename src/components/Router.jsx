94702d0187bf7c678eabf3f41c227fff6693f22b
Lavender Mise <lavendermise4@gmail.com>
2023-08-03 08:29:05 +0300

    Modified the router component

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
- 
-import Shops from "./Shops";
 
+import Shops from "./Shops";
 
 import Signup from "./Signup";
 import Contactus from "./Contactuspage";
 import About from "../pages/About";
 import Cart from "../pages/Cart";
-import Orderpage from "./Orderpage";
 import Payment from "./Payment";
 
 function Router() {
@@ -20,10 +18,9 @@ function Router() {
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
 
-        {/* <Route path="/order" element={<Order/>}/> */}
-        <Route path="/cart" element={< Cart/>}/>
-        <Route path="/order" element={<Orderpage/>}/>
-        {/* <Route path="/cart" element={< Cart/>}/> */} development
+        <Route path="/cart" element={<Cart/>} />
+      
+
         <Route path="contactus" element={<Contactus />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/checkout" element={<Payment />} />
